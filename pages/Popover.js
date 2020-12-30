16b0128a93e9cc0b76ca6f55757edfee0204c5b0
VodeniZeko <edvin.saletovic@pm.me>
2020-12-30 09:15:14 -0800

    clock

--- pages/Popover.js
+++ pages/Popover.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import customStyle from "../styles/customStyles";
 import { Popover, Typography, Button } from "@material-ui/core/";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Popover from "./popover";
+import Popover from "./Popover";
 
 /////////////////////////////////////////////////////////////////////
 
